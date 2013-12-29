5d8affcc8006f0de98aca6dd8ac3861045b928be
Android Dev <codenamegit@gmail.com>
2013-12-29 08:33:09 -0800

    remove old ringtone, alarm, and notification sounds
    will be defined elsewhere

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,12 +39,6 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     PhotoTable
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
